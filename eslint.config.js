605a07ece1ebc9a7706ac27048cc226b69494786
anujithrb <anujith.rb@gmail.com>
2026-04-06 14:48:22 +0530

    chore: add ESLint v9 flat config with angular-eslint and typescript-eslint

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angularEslint = require('angular-eslint');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...angularEslint.configs.tsRecommended,
+    ],
+    processor: angularEslint.processInlineTemplates,
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        { type: 'attribute', prefix: ['ft', 'fta'], style: 'camelCase' },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        { type: 'element', prefix: ['ft', 'fta'], style: 'kebab-case' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angularEslint.configs.templateRecommended,
+      ...angularEslint.configs.templateAccessibility,
+    ],
+    rules: {
+      '@angular-eslint/template/prefer-control-flow': 'error',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '**/dist/',
+      '.angular/',
+      'apps/api/',
+      'apps/admin/',
+      'packages/database/',
+    ],
+  },
+);
